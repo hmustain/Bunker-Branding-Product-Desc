5914c5d6f86cd0d839b0f88401c898b8b7da0c58
hmustain <hunter@bunkerbranding.com>
2023-04-05 14:08:53 -0500

    added back end server files, working on chatGPT controller

---
+++ Server/controllers/chatGPTController.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+require('dotenv').config();
+
+const API_KEY = process.env.CHATGPT_API_KEY;
+
+const generateDescription = async (product) => {
+  try {
+    const response = await router.post(
+      'https://api.openai.com/v1/engines/davinci-codex/completions',
+      {
+        prompt: `Generate a product description for a ${product.category} named ${product.name} that ${product.description}.`,
+        max_tokens: 150,
+        n: 1,
+        stop: '.',
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${API_KEY}`,
+        },
+      }
+    );
+    return response.data.choices[0].text.trim();
+  } catch (error) {
+    console.error(error);
+    return '';
+  }
+};
+
+module.exports = {
+  generateDescription,
+};
