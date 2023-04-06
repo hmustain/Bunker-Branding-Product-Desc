6bc0ccda4d6da6a4878ba999fa5e38abac499a35
hmustain <hunter@bunkerbranding.com>
2023-04-06 10:57:04 -0500

    fixed error with header not styling correectly

--- Client/src/Componets/Header/index.js
+++ Client/src/Componets/Header/index.js
@@ -3,10 +3,10 @@ import './style.css';
 
 const Header = () => {
   return (
-    <header>
-      <img src="./assets/images/BunkerLogo_White.svg" alt="Brand logo" />
+    <header className='header'>
+      <img className='logo' src={process.env.PUBLIC_URL + '/assets/BunkerLogo_White.svg'} alt="Brand logo" />
       <span>
-        Powered by <img className="logo" src="./assets/images/ChatGPT-Logo.jpg" alt="GPT logo" />
+        Powered by <img className="logo" src={process.env.PUBLIC_URL + '/assets/ChatGPT-Logo.jpg'} alt="GPT logo" />
       </span>
     </header>
   );
