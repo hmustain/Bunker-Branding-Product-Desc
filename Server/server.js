ecc217f8dd887898e7af49273c5954a6743a0f9e
hmustain <hunter@bunkerbranding.com>
2023-04-07 09:06:32 -0500

    updating server.js

--- Server/server.js
+++ Server/server.js
@@ -14,8 +14,12 @@ app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, '../client/build/images')));
 
 if (process.env.NODE_ENV === 'production') {
- app.use(express.static(path.join(__dirname, 'client/build')));
+ app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
