6adc257d029178b1ff0c591e7b69033e4768ec01
hmustain <hunter@bunkerbranding.com>
2023-04-07 08:54:14 -0500

    rebuild app

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, '../client/build/images')));
 
 if (process.env.NODE_ENV === 'production') {
- app.use(express.static(path.join(__dirname, '../client/build')));
+ app.use(express.static(path.join(__dirname, 'client/build')));
 }
 
 app.get('/*', (req, res) => {
